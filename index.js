4648ff7eb6c07ceb3af86324442c82a1c7559e34
佐々木翔太郎 <sasakishotaro@sasakishoutarounoMacBook-Air.local>
2021-09-15 17:36:05 +0900

    [Add]Vue.js

---
+++ index.js
@@ -0,0 +1,16 @@
+var app = new Vue({
+  // options
+  el: '#app',
+  data: {
+    toggle: true,
+    message: 'Hello Vue.js!',
+    count: 10,
+    user: {
+      lastName: 'Sasaki',
+      firstName: 'shotaro',
+      age: 28,
+      prefecture: 'Aichi'
+    },
+    colors: ['RED', 'Green', 'Blue']
+  }
+})
